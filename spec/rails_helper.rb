fa7cb0d690ef002e8197e0d53d74337dda27fd88
yukomel <ym02101213@gmail.com>
2022-05-25 14:44:23 +0900

    結合テストの実装

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,7 +20,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -67,5 +67,5 @@ end
 I18n.locale = "en"
 
 RSpec.configure do |config|
-
+  config.include SignInSupport
 end
